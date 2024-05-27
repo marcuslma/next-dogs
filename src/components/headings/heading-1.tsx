0aa0399c637b08300ca0ff639712b2aa309e9bfa
Marcus Maurmann <marcuslma@gmail.com>
2024-05-27 10:25:16 -0300

    remove span as after and apply after on h1

--- src/components/headings/heading-1.tsx
+++ src/components/headings/heading-1.tsx
@@ -4,8 +4,11 @@ export default async function Heading1({
   children: React.ReactNode;
 }>) {
   return (
-    <h1 className="font-spectral leading-none text-[3rem] font-bold my-4 relative z-[1]">
-      <span className="block w-6 h-6 bg-[#fb1] absolute bottom-1 left-[-5px] rounded-[0.2rem] z-[-1]" />
+    <h1
+      className="font-spectral leading-none text-[3rem] font-bold my-4 relative z-[1]
+        after:block after:w-6 after:h-6 after:bg-[#fb1] after:absolute after:bottom-1
+        after:left-[-5px] after:rounded-[0.2rem] after:z-[-1]"
+    >
       {children}
     </h1>
   );
