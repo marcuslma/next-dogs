614f5734a52c7646b28f9ca5d8099f76a1b1c62f
Marcus Maurmann <marcuslma@gmail.com>
2024-06-02 23:52:34 -0300

    fix line height of button and input

--- src/components/buttons/button-link.tsx
+++ src/components/buttons/button-link.tsx
@@ -5,7 +5,7 @@ type ButtonLinkType = LinkProps & React.ComponentProps<"a">;
 
 export default function ButtonLink({ className, ...props }: ButtonLinkType) {
   let buttonClassName =
-    "font-dogs-next-body text-base text-[#764701] cursor-pointer rounded-[0.4rem] border-none bg-[#fb1] min-w-32 py-[0.8rem] px-[1.2rem] box-border transition duration-100 disabled:opacity-50 disabled:cursor-wait after:outline-none hover:shadow-[0_0_0_3px_#fea,0_0_0_4px_#fb1] focus:outline-none focus:shadow-[0_0_0_3px_#fea,0_0_0_4px_#fb1]";
+    "font-dogs-next-body text-base text-[#764701] cursor-pointer rounded-[0.4rem] leading-[1.125rem] border-none bg-[#fb1] min-w-32 py-[0.8rem] px-[1.2rem] box-border transition duration-100 after:outline-none hover:shadow-[0_0_0_3px_#fea,0_0_0_4px_#fb1] focus:outline-none focus:shadow-[0_0_0_3px_#fea,0_0_0_4px_#fb1] disabled:opacity-50 disabled:cursor-wait";
 
   if (className) {
     buttonClassName = `${className} ${buttonClassName}`;
--- src/components/forms/button.tsx
+++ src/components/forms/button.tsx
@@ -5,11 +5,12 @@ type ButtonType = React.ComponentProps<"button">;
 export default function Button(props: ButtonType) {
   return (
     <button
-      className="font-dogs-next-body block text-base text-[#764701] cursor-pointer
-        rounded-[0.4rem] border-none bg-[#fb1] min-w-32 py-[0.8rem] px-[1.2rem]
-        box-border transition duration-100 disabled:opacity-50 disabled:cursor-wait
+      className="font-dogs-next-body block text-base text-[#764701]
+        cursor-pointer rounded-[0.4rem] leading-[1.125rem] border-none bg-[#fb1]
+        min-w-32 py-[0.8rem] px-[1.2rem] box-border transition duration-100
         after:outline-none hover:shadow-[0_0_0_3px_#fea,0_0_0_4px_#fb1]
-        focus:outline-none focus:shadow-[0_0_0_3px_#fea,0_0_0_4px_#fb1]"
+        focus:outline-none focus:shadow-[0_0_0_3px_#fea,0_0_0_4px_#fb1]
+        disabled:opacity-50 disabled:cursor-wait"
       {...props}
     >
       {props.children}
